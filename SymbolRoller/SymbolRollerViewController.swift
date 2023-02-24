90648ffbbde4750128ef4d2feebbc298989a06f0
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2023-02-24 16:27:18 +0900

    - 프로젝트_심볼롤러_03  1. 초기에 보여지는 imageView, label 세팅  2. button을 tap할 시 임의의 imageView, label 세팅  3. reload 메서드(임의의 imageView, label을 세팅하는 기능)를 만들어 재사용할 수 있게함

--- SymbolRoller/SymbolRollerViewController.swift
+++ SymbolRoller/SymbolRollerViewController.swift
@@ -10,41 +10,32 @@ import UIKit
 class SymbolRollerViewController: UIViewController {
     
     let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        imageView.image = UIImage(systemName: "cloud")
         
-        print("view did load...")
+        reload()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("view will appear...")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("view did appear...")
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
-        print("tapped!")
+        reload()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func reload() {
+        let symbol = symbols.randomElement()!
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
     }
-    */
-
 }
