bc7f7e1c25bc7ed5b305ff931cbec73d695ab134
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2023-02-28 16:04:40 +0900

    - 프로젝트 주식리스트 01  1. 샘플 데이터 추가  2. UI 구성(collectionview 추가, CollectionViewCell (label, imageview) 추가

---
+++ StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by 최제환 on 2023/02/28.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
